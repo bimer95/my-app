9e6be4a12f88e1cd3e52cc57a71f7ce1183e8a9d
shevchenko <shevchenkoVEN@yandex.ru>
2020-10-11 09:25:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5XTzK1G6vqmNRc-gwyc-0GxvMAE5_zuuOew&usqp=CAU'></img>
+        </header>
+        <nav className='nav'>
+          <div>
+            <a>Profile</a>
+          </div>
+            <div>
+              <a>Messages</a>
+              </div><div>
+              <a>News</a>
+              </div><div>
+              <a>Music</a>
+              </div><div>
+              <a>Settings</a>
+              </div>
+        </nav>
+        <div className='content'>
+          <div>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTCosNE8U8r4l6IY0NHJ7icaHynM8gFLQimNg&usqp=CAU'></img>
+          </div>
+          <div>
+            ava + descr
+          </div>
+          <div>
+           My posts
+          </div>
+          <div>
+            New post
+          </div>
+          <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+          </div>
+          </div>
+        </div>);
 }
+
+
+
 
 export default App;
