76841b07e769dd3d5847c8ce08e440bfc98b9203
shevchenko <shevchenkoVEN@yandex.ru>
2020-10-21 11:21:57 +0300

    36-37

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,53 +1,58 @@
-let rerenderEntireTree = () =>{
-    console.log('State changed');
-}
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 2, message: 'It\'s my first post', likesCount: 11},
+                {id: 3, message: 'Blabla', likesCount: 11},
+                {id: 4, message: 'Dada', likesCount: 11}
+            ],
+            newPostText: 'rosgvar'
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Veniamin'},
+                {id: 2, name: 'Volod'},
+                {id: 3, name: 'Serge'},
+                {id: 4, name: 'Dmitryy'},
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your?'},
+                {id: 3, message: 'Ok'},
+            ]
+        },
+        sidebar: {}
 
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 12},
-            {id: 2, message: 'It\'s my first post', likesCount: 11},
-            {id: 3, message: 'Blabla', likesCount: 11},
-            {id: 4, message: 'Dada', likesCount: 11}
-        ],
-        newPostText: 'rosgvar'
+
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Veniamin'},
-            {id: 2, name: 'Volod'},
-            {id: 3, name: 'Serge'},
-            {id: 4, name: 'Dmitryy'},
-        ],
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How is your?'},
-            {id: 3, message: 'Ok'},
-        ]
+    getState() {
+        debugger;
+        return this._state
     },
-    sidebar: {}
+    callSubscriber () {
+        console.log('State changed');
+    },
+    addPost () {
+        debugger;
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer;
+    }
+
 }
 
-window.state = state;
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+window.store = store;
+export default store;
