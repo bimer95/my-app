b0b4bffc26dd8f2f4d867fd67571326b9a2e45dd
shevchenko <shevchenkoVEN@yandex.ru>
2020-10-29 19:39:33 +0300

    51-54

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             <div className={`${s.item} ${s.active}`}>
                 <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
                 <div className={`${s.item} ${s.active}`}>
-                <NavLink to="/users" activeClassName={s.activeLink}>Find users</NavLink>
+                <NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
             </div>
             </div>
             <div className={s.item}>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,76 +1,53 @@
 import React from 'react';
-import styles from './users.module.css'
+import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/userman.png';
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1, 
-                photoUrl: 'https://media.tvzvezda.ru/storage/default/2019/01/23/39b2d933d6cc4651b11f61422bc8de98.jpg',
-                followed: true,
-                fullName: 'Veniamin',
-                status: 'admin',
-                location: { city: 'Tomsk', contry: 'Russia' }
-            },
-            {
-                id: 2, 
-                photoUrl: 'https://media.tvzvezda.ru/storage/default/2019/01/23/39b2d933d6cc4651b11f61422bc8de98.jpg',
-                followed: false,
-                fullName: 'Volodya',
-                status: 'user',
-                location: { city: 'Tomsk', contry: 'Russia' }
-            },
-            {
-                id: 3, 
-                photoUrl: 'https://media.tvzvezda.ru/storage/default/2019/01/23/39b2d933d6cc4651b11f61422bc8de98.jpg',
-                followed: false,
-                fullName: 'Sergei',
-                status: 'user',
-                location: { city: 'Roan', contry: 'France' }
-            },
-            {
-                id: 4, 
-                photoUrl: 'https://media.tvzvezda.ru/storage/default/2019/01/23/39b2d933d6cc4651b11f61422bc8de98.jpg',
-                followed: false,
-                fullName: 'Anton',
-                status: 'user',
-                location: { city: 'Tomsk', contry: 'Russia' }
-            }
-        ]
-        )
+class Users extends React.Component {
+
+    
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+                this.props.setUsers(response.data.items); //массив наших пользователей 
+            });
     }
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
-                <span>
-                    <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
-                    </div>
-                    <div>
-                        {u.followed
-                            ? <button onClick={() => {
-                                props.unfollow(u.id)
-                            }}>Unfollow</button>
-                            : <button onClick={() => {
-                                props.follow(u.id)
-                            }}>Follow</button>}
-
-                    </div>
-                </span>
-                <span>
+    render () {
+        return <div>
+        
+            {
+                this.props.users.map(u => <div key={u.id}>
                     <span>
-                        <div>{u.fullName}</div>
-                        <div>{u.status}</div>
+                        <div>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
+                        </div>
+                        <div>
+                            {u.followed
+                                ? <button onClick={() => {
+                                    this.props.unfollow(u.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    this.props.follow(u.id)
+                                }}>Follow</button>}
+    
+                        </div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
+                        </span>
                     </span>
-                </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
+
 
 export default Users;
