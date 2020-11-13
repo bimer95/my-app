3034c3c58216f4fb780e7c6e990443227d766304
shevchenko <shevchenkoVEN@yandex.ru>
2020-11-13 15:26:31 +0300

    merge fix

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -44,7 +44,6 @@ const Login = (props) => {
     if (props.isAuth) {
         return <Redirect to={"/profile"} />
     }
-
     return <div>
         <h1>Login</h1>
         <LoginReduxForm onSubmit={onSubmit} />
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -11,12 +11,13 @@ let initialState = {
     isAuth: false
 };
 
-const authReducer = (state = initialState, action) => {
+const authReducer = (state = initialState, action) => { 
     switch (action.type) {
         case SET_USER_DATA:
             return {
                 ...state,
-                ...action.payload
+                ...action.data,
+                isAuth: true
             }
 
         default:
