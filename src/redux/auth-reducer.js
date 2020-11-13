eebc6ef7417e6cd85a1e432b42003704285d3e60
shevchenko <shevchenkoVEN@yandex.ru>
2020-11-13 15:31:29 +0300

    Update auth-reducer.js

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -16,8 +16,7 @@ const authReducer = (state = initialState, action) => {
         case SET_USER_DATA:
             return {
                 ...state,
-                ...action.data,
-                isAuth: true
+                ...action.data
             }
 
         default:
