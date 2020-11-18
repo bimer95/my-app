8c1d07a343bd329deca14bbf13dd99708c990a55
shevchenko <shevchenkoVEN@yandex.ru>
2020-11-18 19:33:25 +0300

    93 - paginator, постраничный вывод

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -5,43 +5,39 @@ import { NavLink } from 'react-router-dom';
 import Paginator from '../common/Paginator/Paginator';
 
 
-let User = ({ user, followingInProgress, unfollow, follow }) => {
-
+let User = ({user, followingInProgress, unfollow, follow}) => {
     return (
-        <div >
-            <span>
-                <div>
-                    <NavLink to={'/profile/' + user.id}>
-                        <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.userPhoto} />
-
-                    </NavLink>
-                </div>
-                <div>
-                    {user.followed
-                        ? <button disabled={followingInProgress.some(id => id === user.id)}
-                            onClick={() => { unfollow(user.id); }}>
-                            Unfollow</button>
-                        : <button disabled={followingInProgress.some(id => id === user.id)}
-                            onClick={() => { follow(user.id); }}>
-
-
-                            Follow</button>}
-
-                </div>
-            </span>
-            <span>
+       <div>
                 <span>
-                    <div>{user.name}</div>
-                    <div>{user.status}</div>
+                    <div>
+                       <NavLink to={'/profile/' + user.id}>
+                        <img src={user.photos.small != null ? user.photos.small : userPhoto}
+                             className={styles.userPhoto}/>
+                       </NavLink>
+                    </div>
+                    <div>
+                        {user.followed
+                            ? <button disabled={followingInProgress
+                                .some(id => id === user.id)}
+                                      onClick={() => { unfollow(user.id) }}>
+                                Unfollow</button>
+                            : <button disabled={followingInProgress.some(id => id === user.id)}
+                                      onClick={() => { follow(user.id) }}>
+                                      Follow</button>}
+
+                    </div>
                 </span>
                 <span>
-                    <div>{"u.location.country"}</div>
-                    <div>{"u.location.city"}</div>
+                    <span>
+                        <div>{user.name}</div>
+                        <div>{user.status}</div>
+                    </span>
+                    <span>
+                        <div>{"user.location.country"}</div>
+                        <div>{"user.location.city"}</div>
+                    </span>
                 </span>
-            </span>
-        </div> )
+            </div>)
 }
-
-
 
 export default User;
