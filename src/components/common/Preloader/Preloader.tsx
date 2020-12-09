5ce24d97a48ce0651c2e1d55144319cc3e78fa8f
shevchenko <shevchenkoVEN@yandex.ru>
2020-12-09 19:41:15 +0300

    2

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import preloader from '../../../assets/images/preloader.svg'; 
 
+type PropsType = {
+}
 
-let Preloader =(props) =>{
-    return <div style={ {beckgroundColor: 'white'} }>
+let Preloader: React.FC = () => {
+    return <div  style={ { backgroundColor: 'white' } }>
         <img src={preloader} />
-        </div> 
+    </div>
 }
 
 export default Preloader;
