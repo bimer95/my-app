65f7535589eb47274bd86e08a5e3245c92bc7dd2
shevchenko <shevchenkoVEN@yandex.ru>
2020-12-09 14:15:46 +0300

    11 - React + TypeScript

--- src/components/Profile/ProfileInfo/ProfileDataForm.tsx
+++ src/components/Profile/ProfileInfo/ProfileDataForm.tsx
@@ -12,35 +12,37 @@ type PropsType = {
 type ProfileTypeKeys = GetStringKeys<ProfileType>
 
 const ProfileDataForm: React.FC<InjectedFormProps<ProfileType, PropsType> & PropsType> = ({handleSubmit, profile, error}) => {
-    return <form onSubmit = {handleSubmit} >
+    return <form onSubmit={handleSubmit}>
     <div><button>save</button></div>
     {error && <div className={style.formSummaryError}>
-                {error}
-            </div>
-            }
-<div>
-    <b>Full name</b>:  
-    {createField<ProfileTypeKeys> ("Full name", "fullName", [], Input)}
-</div>
-<div>
-    <b>Looking for a job</b>:  
-    {createField<ProfileTypeKeys> ("", "lookingjob", [], Input, {type: "checkbox"})}
-</div>
+        {error}
+    </div>
+    }
     <div>
-        <b>My professional skills</b>:  {createField<ProfileTypeKeys> ("My professional", "lookingForAJodDescription", [], Textarea)}
+        <b>Full name</b>: {createField<ProfileTypeKeys>("Full name", "fullName", [], Input)}
+    </div>
+    <div>
+        <b>Looking for a job</b>: { createField<ProfileTypeKeys>("", "lookingForAJob", [], Input, {type: "checkbox"} )}
     </div>
 
-<div>
-    <b>About me</b>:  
-    { createField<ProfileTypeKeys> ("About me", "about me", [], Textarea)}
-</div>
-<div>
-    <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+    <div>
+        <b>My professional skills</b>:
+        { createField<ProfileTypeKeys>("My professional skills", "lookingForAJobDescription", [], Textarea  )}
+    </div>
+
+
+    <div>
+        <b>About me</b>:
+        { createField<ProfileTypeKeys>("About me", "aboutMe", [], Textarea  )}
+    </div>
+    <div>
+        <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
         return <div key={key} className={s.contact}>
-            <b>{key}: {createField (key, "contacts." + key, [], Input)}</b>
+            {/* todo: create some solution for embedded objects */}
+        <b>{key}: {createField(key, "contacts." + key, [], Input)}</b>
         </div>
     })}
-</div> 
+    </div>
 </form>
 }
 
